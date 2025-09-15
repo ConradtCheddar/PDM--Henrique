849c4f8e8787d09d8ae67dfda12934af45798c4f
ConradtCheddar <henriqueconradt@gmail.com>
2025-09-15 09:14:19 -0300

    o programa quebrou o emulador do android studio

--- app/src/main/java/com/example/app/ImcResultado.java
+++ app/src/main/java/com/example/app/ImcResultado.java
@@ -1,6 +1,10 @@
 package com.example.app;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ImcResultado extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
+    TextView resultado;
+    ImageView imageView;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
+        resultado = findViewById(R.id.resultado);
+        imageView = findViewById(R.id.imageView2);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        double peso = bundle.getDouble("peso");
+        double altura = bundle.getDouble("altura");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    EditText edPeso, edAltura;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button5);
+        button = findViewById(R.id.button);
+        edPeso=findViewById(R.id.editpeso);
         button.setOnClickListener(v -> {
             Intent intent = new Intent(this,ImcResultado.class);
+            Bundle bundle = new Bundle();
+            double peso = Double.parseDouble(edPeso.getText().toString());
+            double altura = Double.parseDouble(edAltura.getText().toString());
+            bundle.putDouble("peso",peso);
+            bundle.putDouble("altura", altura);
+            intent.putExtras(bundle);
             startActivity(intent);
         });
 
