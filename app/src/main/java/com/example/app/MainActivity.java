1db4fca8362445f6c72817b2df9e1d9997d49eac
ConradtCheddar <henriqueconradt@gmail.com>
2025-10-06 09:11:09 -0300

    list view

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.app;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +15,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
-    Button Botao;
-    EditText editNumero;
+
+    ArrayList<String> nomes;
+
+    ListView listview;
+    Button button;
+    EditText editText;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,15 +32,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Botao = findViewById(R.id.Botao);
-        editNumero =findViewById(R.id.editNumero);
-        Botao.setOnClickListener(v -> {
-            Intent intent = new Intent(this, TabuadaResultado.class);
-            Bundle bundle = new Bundle();
-            double numero = Double.parseDouble(editNumero.getText().toString());
-            bundle.putDouble("Numero", numero);
-            intent.putExtras(bundle);
-            startActivity(intent);
+        listview = findViewById(R.id.listview);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        nomes = new ArrayList<>(Arrays.asList("Sigma", "Beta", "Omega"));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,android.R.id.text1, nomes);
+        listview.setAdapter(adapter);
+        listview.setOnClickListener( v ->{
+            nomes.add(editText.getText().toString());
+            adapter.notifyDataSetChanged();-
+        });
+        listview.setOnItemLongClickListener((parent,view,position,id)->{
+            nomes.remove(position);
+            adapter.notifyDataSetChanged();
+            return true;
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
