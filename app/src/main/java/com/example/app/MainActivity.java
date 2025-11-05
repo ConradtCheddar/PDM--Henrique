a8673f3149bea46ad30a75880b4b4424ffe7e2af
ConradtCheddar <henriqueconradt@gmail.com>
2025-11-05 08:19:13 -0300

    contador

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,11 +16,19 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    int cont = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+        textView.setText(Integer.toString(cont));
+        textView.setOnClickListener((View v)-> {
+            textView.setText(Integer.toString(cont++));
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
